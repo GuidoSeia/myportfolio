23ebf3b070c093728544946ebd9181b1ae62a2be
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-11-01 01:07:28 -0300

    agregado de estilo

--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -9,9 +9,9 @@ export default function NavBar() {
 
       return (
       
-            <div className="navbar  justify-evenly align-center ">
+            <div className="navbar justify-evenly align-center ">
                   <li className="btn-nav btn-ghost  normal-case text-xs text-white font-semibold "><LinkRouter to='/'>Home</LinkRouter></li>
-                  <li className="btn-nav btn-ghost  normal-case text-xs text-white font-semibold" onClick={() => scrollTo({ top: 530, left: 0, behavior: "smooth" })} >About</li>
+                  <li className="btn-nav btn-ghost  normal-case text-xs text-white font-semibold" onClick={() => scrollTo({ top: 530, left: 0, behavior: "smooth", block: 'start' })} >About</li>
                   <li className="btn-nav btn-ghost  normal-case text-xs text-white font-semibold">Contact</li>
             </div>
        
