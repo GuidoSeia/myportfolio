8d1ec98c48929be709a359e55be659306efffba3
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-11-11 19:55:51 -0300

    modificaciones en el template

--- myportfolio/src/components/Contact.js
+++ myportfolio/src/components/Contact.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import  "../styles/Contact.css"
+
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <h2 className='techTitle text-2x1 flex w-full items-center justify-center'>CONTACT</h2>
+      <div className='flex '>
+
+        <div className='form-contact w-1/2 bg-slate-100 flex items-center justify-center flex-col'>
+          hola
+          <input type="email" placeholder="" className="input " />
+        </div>
+
+        <div className='other-contact w-1/2 bg-black flex items-center justify-center'>
+          todo ok
+        </div>
+
+      </div>
+    </div>
   )
 }
 
